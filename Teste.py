133cefb2fc5e1327e5ad1a2ab3987e6142a976e9
BAIR <clubesdosul@gmail.com>
2026-03-10 18:13:29 -0300

    Update Teste.py

--- Teste.py
+++ Teste.py
@@ -13,7 +13,7 @@ def calcular_k97_total(eixo_ewz, p_ewz_atual, max_ewz, min_ewz, eixo_dol):
         dolar_vivo = eixo_dol * (1 + (var_atual / 100))
         
         # Cálculo Especial (Divisor 3,6)
-        var_fraja = ((eixo_ewz / p_ewz_atual) - 1) * 100 / 3.6
+        var_fraja = ((eixo_ewz / p_ewz_atual) - 1) * 100 / 5.0
         dolar_fraja = eixo_dol * (1 + (var_fraja / 100))
         
         var_neg = ((eixo_ewz / max_ewz) - 1) * 100 / 2
