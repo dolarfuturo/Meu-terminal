e40d599ad4b2238b33d4d2025234f12805e4452a
BAIR <clubesdosul@gmail.com>
2026-05-08 14:35:58 -0300

    Update layouts.py

--- layouts.py
+++ layouts.py
@@ -115,12 +115,12 @@ def calcular_k97_total(div_spreed, p_ewz_atual, eixo_dol, spot_data, us10y_data)
     try:
         if not spot_data or p_ewz_atual == 0 or not us10y_data: return None
         amp = spot_data['mx'] - spot_data['mn']
-        v_spreed = amp / 2
+        v_spreed = amp / 3
         
         # --- CALIBRAGEM ---
         alvo_low = spot_data['mn'] + v_spreed
         alvo_high = spot_data['mx'] + v_spreed
-        folga = v_spreed / 3
+        folga = v_spreed / 2
         max_original, min_original = eixo_dol + (amp * 0.75), eixo_dol - (amp * 0.25)
         dolar_medio = ((max_original + min_original) / 2) - v_spreed
         
