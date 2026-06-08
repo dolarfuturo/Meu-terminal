520c370f426a465883edcd2acadb9dbd4b8daffb
BAIR <clubesdosul@gmail.com>
2026-06-08 13:26:05 -0300

    Update layouts.py

--- layouts.py
+++ layouts.py
@@ -388,7 +388,7 @@ while True:
                 <div class="calc-panel" style="margin-top: 4px;">
                     <div class="calc-row">
                         <span style="color:#AAA;">PREÇO BASE (8M)</span> 
-                        <span style="color:#ffffff; font-weight: bold;">{res['preco_base_atual']:.4f}</span>
+                        <span style="color:#ffffff; font-weight: bold;">{res['preco_base_atual']:.3f}</span>
                     </div>
                     <div class="calc-row" style="border-bottom: none; margin-top: 2px;">
                         <span style="color:#ffffff;">𝚫 SPOT (FORÇA)</span> 
