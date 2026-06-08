a63f65f09cb14c18d0ff084f496d341f546f36a7
BAIR <clubesdosul@gmail.com>
2026-06-08 17:54:59 -0300

    Update layouts.py

--- layouts.py
+++ layouts.py
@@ -251,8 +251,8 @@ def calcular_k97_total(spreed_do_dia, spot_data, ewz_data):
         fracao_4s = (preco_spot_atual - st.session_state.k97_abertura_base) / 10
         st.session_state.k97_delta_acumulado = st.session_state.k97_base_forca_ciclo + fracao_4s
 
-        # 🔄 RETORNO DA CONTA DE 4S ORIGINAL: Oscilando dinamicamente ao vivo a cada ciclo!
-        saida_indicador_tela = (preco_spot_atual - preco_open_oficial) / 4 / 10
+        # 🔄 ALTERAÇÃO SOLICITADA: Preço Atual - Preço Base / 10 (Sem divisão por 4)
+        saida_indicador_tela = (preco_spot_atual - st.session_state.k97_abertura_base) / 10
 
         output_res = {
             "white": True, "vivo": vivo_val, "vivo_pct": calc_variacoes_pct * 100, "dolfut_calc": dolfut_atual_calc, "fraja": fraja_val, 
