fa753c10c56b49c18b69375e0c2a860f63b65934
BAIR <clubesdosul@gmail.com>
2026-07-06 14:06:32 -0300

    Update layouts.py

--- layouts.py
+++ layouts.py
@@ -424,8 +424,8 @@ while True:
                 # Cores dos blocos
                 c_bf, c_b, c_n, c_a, c_af = "", "", "", "", ""
                 if media_term <= -1.00: c_bf = "active-bf"
-                elif media_term < -0.35: c_b = "active-b"
-                elif media_term <= 0.35: c_n = "active-n"
+                elif media_term < -0.25: c_b = "active-b"
+                elif media_term <= 0.25: c_n = "active-n"
                 elif media_term < 1.00: c_a = "active-a"
                 else: c_af = "active-af"
                 
