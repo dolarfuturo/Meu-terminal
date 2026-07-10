ef74f1adf3e955636675027da5d44681011175d8
BAIR <clubesdosul@gmail.com>
2026-07-10 09:16:27 -0300

    Change p_min and p_max values for media term
    Adjust the range for p_min and p_max in layout calculations.

--- layouts.py
+++ layouts.py
@@ -431,7 +431,7 @@ while True:
                 elif media_term < 0.60: c_a = "active-a"
                 else: c_af = "active-af"
                 
-                p_min, p_max = -1.0, 1.0
+                p_min, p_max = -1.5, 1.5
                 pos_percent = ((media_term - p_min) / (p_max - p_min)) * 100
                 pos_percent = max(0, min(100, pos_percent)) 
                 
