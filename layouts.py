0d98ca71eb6a6343df8403c8d5dd6c24f7e8723d
BAIR <clubesdosul@gmail.com>
2026-07-15 11:01:46 -0300

    Update layouts.py

--- layouts.py
+++ layouts.py
@@ -423,7 +423,8 @@ while True:
                 v_us10y = get_var("^TNX")
                 v_zn_f = get_var("ZN=F")
                 
-                media_term = v_dxy - v_zn_f - (v_ewz * 0.3) / 3
+                media_term = (v_dxy - v_zn_f - (v_ewz * 0.30)) / 3
+
                 
                 c_bf, c_b, c_n, c_a, c_af = "", "", "", "", ""
                 if media_term <= -0.60: c_bf = "active-bf"
