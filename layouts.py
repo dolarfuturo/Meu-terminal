8b1f67efaf4809cc32450c56484439c2647bcbb1
BAIR <clubesdosul@gmail.com>
2026-07-22 16:43:28 -0300

    Update layouts.py

--- layouts.py
+++ layouts.py
@@ -432,7 +432,7 @@ while True:
                 elif media_term < 0.60: c_a = "active-a"
                 else: c_af = "active-af"
                 
-                p_min, p_max = -3.0, 3.0
+                p_min, p_max = -2.0, 2.0
                 pos_percent = ((media_term - p_min) / (p_max - p_min)) * 100
                 pos_percent = max(0, min(100, pos_percent)) 
                 
