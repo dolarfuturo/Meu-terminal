9f05e285a3337d080e37945029868d9fbc55e7b1
BAIR <clubesdosul@gmail.com>
2026-07-23 13:02:43 -0300

    Update layouts.py

--- layouts.py
+++ layouts.py
@@ -423,7 +423,7 @@ while True:
                 v_us10y = get_var("^TNX")
                 v_zn_f = get_var("ZN=F")
                 
-                media_term = (v_dxy) - (v_ewz) * 0.1
+                media_term = (v_dxy - v_ewz) * 0.1
                 
                 c_bf, c_b, c_n, c_a, c_af = "", "", "", "", ""
                 if media_term <= -0.50: c_bf = "active-bf"
@@ -462,7 +462,7 @@ while True:
                         return v
                     return 0.0
 
-                media_term_c2 = get_var_local("DX-Y.NYB") - get_var_local("EWZ") * 0.1 
+                media_term_c2 = (get_var_local("DX-Y.NYB") - get_var_local("EWZ")) * 0.1 
 
                 axis_plus_term = res['axis_central'] * (1 + (media_term_c2 / 100))
 
